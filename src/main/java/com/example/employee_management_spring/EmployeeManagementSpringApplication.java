9f2bbf760ba479a655b327487fcfc926d85fc673
tungnk-2476 <nguyen.khac.tung@sun-asterisk.com>
2026-07-27 16:16:57 +0700

    Init poject module 1

---
+++ src/main/java/com/example/employee_management_spring/EmployeeManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_management_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employee_management_spring/EmployeeManagementSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_management_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
