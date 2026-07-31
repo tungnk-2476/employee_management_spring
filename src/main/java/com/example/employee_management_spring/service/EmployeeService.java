acc88bdbad91356b914de5a3ff36f4820f0b9b5e
tungnk-2476 <nguyen.khac.tung@sun-asterisk.com>
2026-07-31 10:13:34 +0700

    module 7

--- src/main/java/com/example/employee_management_spring/service/EmployeeService.java
+++ src/main/java/com/example/employee_management_spring/service/EmployeeService.java
@@ -1,6 +1,9 @@
 package com.example.employee_management_spring.service;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import com.example.employee_management_spring.exception.ResourceNotFoundException;
 import com.example.employee_management_spring.model.Department;
@@ -13,6 +16,7 @@ import com.example.employee_management_spring.repository.EmployeeRepository;
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final DepartmentRepository departmentRepository;
+    private static final Logger log = LoggerFactory.getLogger(EmployeeService.class);
 
     public EmployeeService(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository) {
         this.employeeRepository = employeeRepository;
@@ -26,7 +30,12 @@ public class EmployeeService {
         employee.setEmail(request.getEmail());
         employee.setDepartment(department);
 
-        return employeeRepository.save(employee);
+        Employee savedEmployee = employeeRepository.save(employee);
+        log.info(
+                "Created employee: id={}, departmentId={}",
+                savedEmployee.getId(),
+                department.getId());
+        return savedEmployee;
     }
 
     public List<Employee> searchByName(String name) {
@@ -55,7 +64,14 @@ public class EmployeeService {
         employee.setEmail(request.getEmail());
         employee.setDepartment(department);
 
-        return employeeRepository.save(employee);
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        log.info(
+                "Updated employee: id={}, departmentId={}",
+                savedEmployee.getId(),
+                department.getId());
+
+        return savedEmployee;
     }
 
     public void deleteById(Integer id) {
@@ -64,6 +80,7 @@ public class EmployeeService {
         }
 
         employeeRepository.deleteById(id);
+        log.info("Deleted employee: id={}", id);
     }
 
     public List<Employee> search(String name, String department) {
